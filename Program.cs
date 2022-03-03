fe21ee47552ad42a9a94f5a70d57884290db7094
dute0987 <dute0987@gmail.com>
2022-03-03 11:34:07 +0530

    program.cs file for add new contact program

--- Program.cs
+++ Program.cs
@@ -8,13 +8,13 @@ namespace AddressBookSystem
             //Console.WriteLine("Welcome To the AddressBookSystem");
 
             //UC-1-AddressBookSystem
-              UC_1_Contact UC1 = new UC_1_Contact();
+            //UC_1_Contact UC1 = new UC_1_Contact();
             //UC1.AddressBook();
-              UC1.Address("vishal","Dute","jalamb","shegaon","MH","444303","9146508505","dute0987@gmail.com");
+            //UC1.Address("vishal","Dute","jalamb","shegaon","MH","444303","9146508505","dute0987@gmail.com");
 
             //UC-2-Add new contact
-            //UC_2_AddNewContact UC2 = new UC_2_AddNewContact();
-            //UC2.NewDetailed();
+              UC_2_AddNewContact UC2 = new UC_2_AddNewContact();
+              UC2.NewDetailed();
 
             //UC-3-Edit Data
             //UC_3_EditData UC3 = new UC_3_EditData();
