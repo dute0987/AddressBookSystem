93869775524065a71e25148fbf2723235b10384f
dute0987 <dute0987@gmail.com>
2022-03-03 11:37:57 +0530

    Main file for Editing data progarm

---
+++ UC-3-EditData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class UC_3_EditData
+    {
+        public void DataEdit()
+        {
+            string firstName;
+            string lastName;
+            string address;
+            string city;
+            string state;
+            int pinCode;
+            long phoneNumber;
+            string email;
+
+            Console.WriteLine("Edit your first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Edit your last name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("Edit your address:");
+            address = Console.ReadLine();
+            Console.WriteLine("Edit your city:");
+            city = Console.ReadLine();
+            Console.WriteLine("Edit your state:");
+            state = Console.ReadLine();
+            Console.WriteLine("Edit zip code:");
+            pinCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Edit phone number:");
+            phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Edit your email:");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Hello" + firstName + " " + lastName + " your details has updated");
+
+        }
+    }
+}
